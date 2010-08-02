b81b7bc0e9e732428808665e7a41d3ecec47c198
aarongustafson <aaron@easy-designs.net>
2010-08-02 08:28:58 -0400

    All tests complete and minified version created.

--- src/Squirrel.js
+++ src/Squirrel.js
@@ -7,9 +7,17 @@ Homepage:      http://github.com/easy-designs/Squirrel.js
 License:       MIT License (see homepage)
 ------------------------------------------------------------------------------*/
 
+/**
+ * Squirrel()
+ * a simple cache manager
+ *
+ * @param str id - a unique id for your cache
+ * @param int seconds - how many seconds you want the cache to live (optional, IE only)
+ * @return obj - a Squirrel instance
+ *
+ */
 function Squirrel( id, seconds )
 {
-  this.version = '1.0';
   
   var
   __cache_object,
@@ -17,14 +25,25 @@ function Squirrel( id, seconds )
   WINDOW  = window,
   HYPHEN  = '-',
   SUB     = 'squirrelsubcache-',
-  div     = document.createElement('div'),
+  DIV     = document.createElement('div'),
+  FUNC    = function(){},
   
+  // internal functions
+  readFromCache   = FUNC,
+  writeToCache    = FUNC,
+  removeFromCache = FUNC,
+
   // IE requires an expiration period
   expire  = new Date();
   seconds = seconds || 10080; // default expiration == 1 week
   expire.setMinutes( expire.getMinutes() + seconds );
   expire  = expire.toUTCString();
   
+  // public stuff
+  this.version = '1.0';
+  this.active  = false;
+  this.clear = this.read = this.write = this.remove = FUNC;
+    
   function argTest( required, provided, message )
   {
     if ( provided < required )
@@ -37,28 +56,6 @@ function Squirrel( id, seconds )
   if ( WINDOW.localStorage != UNDEFINED )
   {
     __cache_object = WINDOW.localStorage;
-    clearCache = function()
-    {
-      var
-      key,
-      i     = __cache_object.length,
-      args  = arguments,
-      cache = id;
-      if ( args.length == 1 )
-      {
-        cache += HYPHEN + SUB + args[0];
-      }
-      // cherry-pick only our own items in the cache
-      while ( i-- )
-      {
-        key = localStorage.key(i);
-        if ( key.indexOf( cache ) === 0 )
-        {
-          delete( __cache_object[key] );
-        }
-      }
-      return true;
-    };
     readFromCache = function( key )
     {
       key = id + HYPHEN + key;
@@ -70,41 +67,34 @@ function Squirrel( id, seconds )
       __cache_object.setItem( key, value );
       return true;
     };
+    removeFromCache = function( cache )
+    {
+      var
+      key,
+      i     = __cache_object.length,
+      cache = cache ? id + HYPHEN + cache : id;
+      while ( i-- )
+      {
+        key = __cache_object.key(i);
+        if ( key.indexOf( cache ) === 0 )
+        {
+          delete( __cache_object[key] );
+        }
+      }
+      return true;
+    };
+    this.active = true;
   }
   // IE (old school)
   else
   {
-    div.style.behavior = 'url(#default#userData)';
-    document.body.appendChild( div );
-    if ( div.XMLDocument != UNDEFINED )
+    DIV.style.behavior = 'url(#default#userData)';
+    document.body.appendChild( DIV );
+    if ( DIV.XMLDocument != UNDEFINED )
     {
-      __cache_object = div;
+      __cache_object = DIV;
       __cache_object.load( id );
       __cache_object.expires = expire;
-      clearCache = function()
-      {
-        var
-        key,
-        cache = false,
-        args  = arguments,
-        attr  = __cache_object.XMLDocument.firstChild.attributes,
-        i     = attr.length;
-        if ( args.length == 1 )
-        {
-          cache = SUB + args[0];
-        }
-        while ( i-- )
-        {
-          key = attr[i].nodeName;
-          if ( ! cache ||
-               key.nodeName.indexOf( cache ) === 0 )
-          {
-            __cache_object.removeAttribute( key );
-          }
-        }
-        __cache_object.save( id );
-        return true;
-      };
       readFromCache = function( key )
       {
         return __cache_object.getAttribute( key );
@@ -115,118 +105,153 @@ function Squirrel( id, seconds )
         __cache_object.save( id );
         return true;
       };
+      removeFromCache = function( cache )
+      {
+        var 
+        key,
+        cache = cache || false,
+        attr  = __cache_object.XMLDocument.firstChild.attributes,
+        i     = attr.length;
+        while ( i-- )
+        {
+          key = attr[i].nodeName;
+          if ( ! cache ||
+               key.indexOf( cache ) === 0 )
+          {
+            __cache_object.removeAttribute( key );
+          }
+        }
+        __cache_object.save( id );
+        return true;
+      };
+      this.active = true;
     }
   }
   
-  /**
-   * Squirrel.clear()
-   * Clears the cache
-   *
-   * Option 1: Clear the entire cache
-   *   Squirrel.clear()
-   *   @return bool - TRUE for success, FALSE for failure
-   *
-   * Option 2: Clear a sub-cache
-   *   Squirrel.clear( sub_cache )
-   *   @param str sub_cache - the sub-cache you'd like to clear
-   *   @return bool - TRUE for success, FALSE for failure
-   *
-   */
-  this.clear = clearCache;
-
-  /**
-   * Squirrel.read()
-   * Reads a key from the cache
-   *
-   * Option 1: Read a key
-   *   Squirrel.read( key )
-   *   @param str key - the key to read
-   *   @return str - the value of the key in the cache
-   *
-   * Option 2: Property Test (simple) 
-   *   Squirrel.read( sub_cache. key )
-   *   @param str sub_cache - the sub-cache you'd like to read the key from
-   *   @param str key - the key to read
-   *   @return str - the value of the key in the sub-cache
-   *
-   */
-  this.read = function()
+  if ( this.active )
   {
-    var
-    args  = arguments,
-    aLen  = args.length,
-    key   = args[0];
-    argTest( 1, aLen, 'Squirrel.read() requires at least one argument.' );
-    if ( args.length == 2 )
+    
+    /**
+     * Squirrel.clear()
+     * Clears the cache
+     *
+     * Option 1: Clear the entire cache
+     *   Squirrel.clear()
+     *   @return bool - TRUE for success, FALSE for failure
+     *
+     * Option 2: Clear a sub-cache
+     *   Squirrel.clear( sub_cache )
+     *   @param str sub_cache - the sub-cache you'd like to clear
+     *   @return bool - TRUE for success, FALSE for failure
+     *
+     */
+    this.clear = function()
     {
-      key = SUB + key + HYPHEN + args[1];
-    }
-    return readFromCache( key );
-  };
+      var
+      sub_cache = false,
+      args      = arguments;
+      if ( args.length == 1 )
+      {
+        sub_cache = SUB + args[0];
+      }
+      return removeFromCache( sub_cache );
+    };
 
-  /**
-   * Squirrel.write()
-   * adds data to the cache
-   *
-   * Option 1: Add a value
-   *   Squirrel.write( key, value )
-   *   @param str key - the key to add
-   *   @param str value - the value to write for key
-   *   @return bool - TRUE for success, FALSE for failure
-   *
-   * Option 1: Add a value to a sub-cache
-   *   Squirrel.write( sub_cache, key, value )
-   *   @param str sub_cache - the sub-cache you'd like to add the key to
-   *   @param str key - the key to add
-   *   @param str value - the value to write for key
-   *   @return bool - TRUE for success, FALSE for failure
-   *
-   */
-  this.write = function()
-  {
-    var
-    args  = arguments,
-    aLen  = args.length,
-    key   = args[0],
-    value = args[1];
-    argTest( 2, aLen, 'Squirrel.write() requires at least two arguments.' );
-    if ( args.length == 3 )
+    /**
+     * Squirrel.read()
+     * Reads a key from the cache
+     *
+     * Option 1: Read a key
+     *   Squirrel.read( key )
+     *   @param str key - the key to read
+     *   @return str - the value of the key in the cache
+     *
+     * Option 2: Property Test (simple) 
+     *   Squirrel.read( sub_cache. key )
+     *   @param str sub_cache - the sub-cache you'd like to read the key from
+     *   @param str key - the key to read
+     *   @return str - the value of the key in the sub-cache
+     *
+     */
+    this.read = function()
     {
-      key   = SUB + key + HYPHEN + args[1];
-      value = args[2];
-    }
-    writeToCache( key, value );
-    return true;
-  };
+      var
+      args  = arguments,
+      aLen  = args.length,
+      key   = args[0];
+      argTest( 1, aLen, 'Squirrel.read() requires at least one argument.' );
+      if ( args.length == 2 )
+      {
+        key = SUB + key + HYPHEN + args[1];
+      }
+      return readFromCache( key );
+    };
 
-  /**
-   * Squirrel.remove()
-   * removes specific data from the cache
-   *
-   * Option 1: Remove a key
-   *   Squirrel.remove( key )
-   *   @param str key - the key to remove
-   *   @return bool - TRUE for success, FALSE for failure
-   *
-   * Option 2: Property Test (simple) 
-   *   Squirrel.remove( sub_cache, key )
-   *   @param str sub_cache - the sub-cache you'd like to remove the key from
-   *   @param str key - the key to remove
-   *   @return bool - TRUE for success, FALSE for failure
-   *
-   */
-  this.remove  = function()
-  {
-    var
-    args = arguments,
-    aLen = args.length,
-    key  = args[0];
-    argTest( 1, aLen, 'Squirrel.remove() requires at least one argument.' );
-    if ( aLen == 2 )
+    /**
+     * Squirrel.write()
+     * adds data to the cache
+     *
+     * Option 1: Add a value
+     *   Squirrel.write( key, value )
+     *   @param str key - the key to add
+     *   @param str value - the value to write for key
+     *   @return bool - TRUE for success, FALSE for failure
+     *
+     * Option 1: Add a value to a sub-cache
+     *   Squirrel.write( sub_cache, key, value )
+     *   @param str sub_cache - the sub-cache you'd like to add the key to
+     *   @param str key - the key to add
+     *   @param str value - the value to write for key
+     *   @return bool - TRUE for success, FALSE for failure
+     *
+     */
+    this.write = function()
     {
-      key = SUB + key + HYPHEN + args[1];
-    }
-    return clearCache( key );
-  };
+      var
+      args  = arguments,
+      aLen  = args.length,
+      key   = args[0],
+      value = args[1];
+      argTest( 2, aLen, 'Squirrel.write() requires at least two arguments.' );
+      if ( args.length == 3 )
+      {
+        key   = SUB + key + HYPHEN + args[1];
+        value = args[2];
+      }
+      writeToCache( key, value );
+      return true;
+    };
+
+    /**
+     * Squirrel.remove()
+     * removes specific data from the cache
+     *
+     * Option 1: Remove a key
+     *   Squirrel.remove( key )
+     *   @param str key - the key to remove
+     *   @return bool - TRUE for success, FALSE for failure
+     *
+     * Option 2: Property Test (simple) 
+     *   Squirrel.remove( sub_cache, key )
+     *   @param str sub_cache - the sub-cache you'd like to remove the key from
+     *   @param str key - the key to remove
+     *   @return bool - TRUE for success, FALSE for failure
+     *
+     */
+    this.remove  = function()
+    {
+      var
+      args = arguments,
+      aLen = args.length,
+      key  = args[0];
+      argTest( 1, aLen, 'Squirrel.remove() requires at least one argument.' );
+      if ( aLen == 2 )
+      {
+        key = SUB + key + HYPHEN + args[1];
+      }
+      return removeFromCache( key );
+    };
+
+  }
   
 }
--- tests/js/test.js
+++ tests/js/test.js
@@ -7,40 +7,75 @@ $(document).ready(function(){
   test( 'Squirrel', function(){
     ok( typeof(Squirrel)!='undefined', 'Squirrel object exists' );
     ok( true, 'Squirrel version being tested: ' + $S.version );
+    if ( $S.active )
+    {
+      ok( true, 'Squirrel is active' );
+    }
+    else
+    {
+      ok( true, 'Squirrel is not active (caching isn\'t available)' );
+    }
   });
   
-  test( 'Squirrel::write( key, value )', function(){
-    ok( typeof($S.write)=='function', 'method exists' );
-    ok( $S.write( 'do', 're' ), 'Wrote value "re" for cached key "do"' );
-  });
-  
-  test( 'Squirrel::write( sub_cache, key, value )', function(){
-    ok( typeof($S.write)=='function', 'method exists' );
-    ok( $S.write( 'test', 'me', 'fa' ), 'Wrote value "fa" for cached key "me" in sub-cache "test"' );
-  });
-  
-  test( 'Squirrel::read( key )', function(){
-    ok( typeof($S.read)=='function', 'method exists' );
-    ok( $S.read( 'do' ) == 're', 'Value of "re" was returned when requesting "do"' );
-  });
-  
-  test( 'Squirrel::read( sub_cache, key )', function(){
-    ok( typeof($S.read)=='function', 'method exists' );
-    ok( $S.read( 'test', 'me' ) == 'fa', 'Value of "fa" was returned when requesting "me" from sub-cache "test"' );
-  });
-  
-  test( 'Squirrel::clear( sub_cache )', function(){
-    ok( typeof($S.clear)=='function', 'method exists' );
-    $S.clear( 'test' );
-    ok( $S.read( 'test', 'me' ) == null, '"me" key no longer exists in the sub-cache "test" in local storage' );
-    ok( $S.read( 'do' ) == 're', 'Value of "re" was still returned when requesting "do"' );
-  });
-  
-  test( 'Squirrel::clear()', function(){
-    ok( typeof($S.clear)=='function', 'method exists' );
-    $S.clear();
-    ok( $S.read( 'test', 'me' ) == null, '"me" key no longer exists in the sub-cache "test" in local storage' );
-    ok( $S.read( 'do' ) == null, '"do" key no longer exists in local storage' );
-  });
+  if ( $S.active )
+  {
+
+    test( 'Squirrel::write( key, value )', function(){
+      ok( typeof($S.write)=='function', 'method exists' );
+      ok( $S.write( 'doe', 'ray' ), 'Wrote value "ray" for cached key "doe"' );
+    });
+
+    test( 'Squirrel::write( sub_cache, key, value )', function(){
+      ok( typeof($S.write)=='function', 'method exists' );
+      ok( $S.write( 'test', 'me', 'fah' ), 'Wrote value "fah" for cached key "me" in sub-cache "test"' );
+    });
+
+    test( 'Squirrel::read( key )', function(){
+      ok( typeof($S.read)=='function', 'method exists' );
+      ok( $S.read( 'doe' ) == 'ray', 'Value of "ray" was returned when requesting "doe"' );
+    });
+
+    test( 'Squirrel::read( sub_cache, key )', function(){
+      ok( typeof($S.read)=='function', 'method exists' );
+      ok( $S.read( 'test', 'me' ) == 'fah', 'Value of "fah" was returned when requesting "me" from sub-cache "test"' );
+    });
+
+    test( 'Squirrel::remove( key )', function(){
+      ok( typeof($S.remove)=='function', 'method exists' );
+      ok( $S.remove( 'doe' ), 'Called Squirrel::remove on key "doe"' );
+      ok( $S.read( 'doe' ) == null, '"doe" key no longer exists in local storage' );
+    });
+
+    test( 'Squirrel::remove( sub_cache, key )', function(){
+      ok( typeof($S.remove)=='function', 'method exists' );
+      ok( $S.remove( 'test', 'me' ), 'Called Squirrel::remove on key "me" in sub-cache "test"' );
+      ok( $S.read( 'test', 'me' ) == null, '"me" key no longer exists in the sub-cache "test" in local storage' );
+    });
+
+    test( 'Squirrel::clear( sub_cache )', function(){
+      ok( typeof($S.clear)=='function', 'method exists' );
+      ok( $S.write( 'doe', 'ray' ), 'Wrote value "ray" for cached key "doe"' );
+      ok( $S.write( 'me', 'fah' ), 'Wrote value "fah" for cached key "me"' );
+      ok( $S.write( 'test', 'so', 'la' ), 'Wrote value "la" for cached key "so" in sub-cache "test"' );
+      ok( $S.write( 'test', 'tee', 'doe' ), 'Wrote value "doe" for cached key "tee" in sub-cache "test"' );
+      ok( $S.clear( 'test' ), 'Called Squirrel::clear on sub-cache "test"' );
+      ok( $S.read( 'test', 'so' ) == null, '"so" key no longer exists in the sub-cache "test" in local storage' );
+      ok( $S.read( 'test', 'tee' ) == null, '"tee" key no longer exists in the sub-cache "test" in local storage' );
+      ok( $S.read( 'doe' ) == 'ray', 'Value of "ray" was still returned when requesting "doe"' );
+      ok( $S.read( 'me' ) == 'fah', 'Value of "fah" was still returned when requesting "doe"' );
+    });
+
+    test( 'Squirrel::clear()', function(){
+      ok( typeof($S.clear)=='function', 'method exists' );
+      ok( $S.write( 'test', 'so', 'la' ), 'Wrote value "la" for cached key "so" in sub-cache "test"' );
+      ok( $S.write( 'test', 'tee', 'doe' ), 'Wrote value "doe" for cached key "tee" in sub-cache "test"' );
+      ok( $S.clear(), 'Called Squirrel::clear' );
+      ok( $S.read( 'test', 'so' ) == null, '"so" key no longer exists in the sub-cache "test" in local storage' );
+      ok( $S.read( 'test', 'tee' ) == null, '"tee" key no longer exists in the sub-cache "test" in local storage' );
+      ok( $S.read( 'doe' ) == null, '"doe" key no longer exists in local storage' );
+      ok( $S.read( 'me' ) == null, '"doe" key no longer exists in local storage' );
+    });
+    
+  }
 
 });
